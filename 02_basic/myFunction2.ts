b7f850d4278db28b579e2622b19b6a5ca5c7e762
Anurag Gupta <anurag6967@gmail.com>
2024-06-18 20:42:27 +0530

    function in typescript with return type

--- 02_basic/myFunction2.ts
+++ 02_basic/myFunction2.ts
@@ -1,13 +1,42 @@
 //while taking argument we checked argument type and 
 //while return we checked the return type
 function addTwo(num: number): number{
-    return num+2
+    return num+2;
+}
+
+//arrow function for addition
+const add = (n: number) :number =>{
+    return n+1;
 }
 
 //arrow function
 const getName = (name:string): string =>{
-    return name
+    return name;
 }
 
-addTwo(4)
-getName("Anurag")
+//map method
+const heros = ["superman","batman","spiderman","ironman"]
+
+const data = heros.map((hero): string =>{
+
+    return `hero are ${hero}`
+})
+
+
+// void return
+function display(msg: string): void{
+    console.log(`msg are ${msg}`)
+}
+// never return
+function handleError(msg: string): never{
+    throw new Error(msg)
+}
+
+addTwo(4);
+add(5);
+getName("Anurag");
+display("message void keyword as return ")
+handleError("never keyword as return ")
+
+
+export {}
