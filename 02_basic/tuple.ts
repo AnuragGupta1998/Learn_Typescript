e266ad76b69eaebe63fe69a5214b2e5e2d320084
Anurag Gupta <anurag6967@gmail.com>
2024-06-19 17:06:59 +0530

    learn about tuples in typescript

---
+++ 02_basic/tuple.ts
@@ -0,0 +1,12 @@
+let User: [string,number,boolean] =["1",2,true]
+
+let rgb: [number,number,number] = [244,190,120]
+
+type User2 =[number,string]
+
+let newUser: User2 =  [11,"anurag"]
+
+newUser[1]="Ashutosh" //overriting the tuple value
+
+
+// newUser.push(false) //not applicable in typescript 5 version 
