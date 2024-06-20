08e102724dc6b831a38f27c9c7ba66be0ad3477b
Anurag Gupta <anurag6967@gmail.com>
2024-06-20 17:21:37 +0530

    learn generic in typescript

---
+++ 03_advance/src/Generic.ts
@@ -0,0 +1,36 @@
+//generic allow us to write reusuable code 
+
+//without generic
+function f(val: string | number | boolean){
+    return val
+}
+
+function f1<Type>(val: Type): Type {
+    return val
+}
+
+f1("2")
+f1(2)
+f1("false")
+
+//another generic
+function f2<T>(val: T): T{
+    return val
+}
+
+f2("2")
+f2(2)
+f2("false")
+
+//array of number and string
+const num: Array<number>=[]
+const num1: Array<string>=[]
+
+//another generic
+interface User{
+    name:string;
+    age:number;
+}
+function f3<User>({}){
+
+}
